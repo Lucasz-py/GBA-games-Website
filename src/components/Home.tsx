c06ec6e4b90088a54cec23a3b57ced62587ca287
Lucasz-27 <escobarlucas27.10@gmail.com>
2025-11-09 22:11:28 -0300

    update style 2.0

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () => {
     const navigate = useNavigate()
 
-    const handleStart = () => {
+    const handleStartClick = () => {
         navigate('/catalogo')
     }
 
@@ -12,12 +12,17 @@ const Home = () => {
         <div className="home">
             <div className="home-content">
                 <h1 className="home-title">
-                    Play the best games for GameBoy Advance
+                    play the best games <br /> for gameboy advance
                 </h1>
-
-                <button className="home-button" onClick={handleStart}>
+                <button className="home-button" onClick={handleStartClick}>
                     Start
                 </button>
+            </div>
+
+            {/* Contenedor para los GIFs decorativos */}
+            <div className="gif-decorations">
+                <img src="/gif3.gif" alt="Link GIF" className="gif-bottom-left" />
+                <img src="/gif1.gif" alt="Pokemon GIF" className="gif-bottom-right" />
             </div>
         </div>
     )
