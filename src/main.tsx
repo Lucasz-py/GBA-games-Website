3f8798a9161725f6fdc4a674b9c8d8f543f5453e
Lucasz-27 <escobarlucas27.10@gmail.com>
2025-11-14 02:35:20 -0300

    test2

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
-import { AuthProvider } from './context/AuthContext.tsx' // <--- IMPORTAR
+// --- 1. IMPORTAR EL PROVIDER ---
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider> {/* <--- ENVOLVER LA APP */}
+      {/* --- 2. ENVOLVER LA APP --- */}
+      <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
